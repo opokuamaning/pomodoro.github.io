40b6f757f54c5a8e2f2d2ebd78f8a47cdfc3dd8f
opokuamaning <prince.amaning@amalitech.org>
2022-07-19 12:28:19 +0000

    Added the modal feature

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-const settingsBtn = document.querySelector(".settings-icon");
+const viewBtn = document.querySelector(".fa-gear");
 const popup = document.querySelector(".popup");
 const close = popup.querySelector(".close-btn");
 
-settingsBtn.addEventListener("click", () =>{
+viewBtn.addEventListener("click", () =>{
     popup.classList.toggle("show");
 });
 close.addEventListener("click", ()=> {
-    settingsBtn.click();
+    viewBtn.click();
 });
 
 
